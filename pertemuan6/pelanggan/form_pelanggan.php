b444c92e47ed76c67998410073aa5a506d8a21f4
ImeldaWahyuni <imeldawahyuni96@gmail.com>
2023-04-02 20:21:12 +0700

    pelanggan

--- pertemuan6/pelanggan/form_pelanggan.php
+++ pertemuan6/pelanggan/form_pelanggan.php
@@ -2,6 +2,22 @@
 require_once '../dbkoneksi.php';
 ?>
 
+<?php 
+    // cek apakah terdapat parameter id pada URL, jika ada maka dilakukan edit data
+    $_id = isset($_GET['idedit']) ? $_GET['idedit'] : null;
+    if(!empty($_id)){
+        // ambil data pelanggan berdasarkan id
+        $sql = "SELECT * FROM pelanggan WHERE id=?";
+        $st = $dbh->prepare($sql);
+        $st->execute([$_id]);
+        $row = $st->fetch();
+    }else{
+        // jika tidak ada parameter id pada URL, maka dianggap input data baru
+        // inisialisasi variabel $row sebagai array kosong
+        $row = [];
+    }
+?>
+
 <form method="POST" action="proses_pelanggan.php">
     <div class="form-group row">
         <label for="kode" class="col-4 col-form-label">Kode</label>
@@ -12,7 +28,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-anchor"></i>
                     </div>
                 </div>
-                <input id="kode" name="kode" type="text" class="form-control" value="">
+                <input id="kode" name="kode" type="text" class="form-control" value="<?php if(isset($row['kode'])) echo $row['kode']; ?>">
             </div>
         </div>
     </div>
@@ -25,7 +41,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-adjust"></i>
                     </div>
                 </div>
-                <input id="nama" name="nama" type="text" class="form-control" value="">
+                <input id="nama" name="nama" type="text" class="form-control" value="<?php if(isset($row['nama'])) echo $row['nama']; ?>">
             </div>
         </div>
     </div>
@@ -38,7 +54,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-arrow-circle-o-left"></i>
                     </div>
                 </div>
-                <input id="jk" name="jk" value="L" type="radio" class="form-control">Laki-laki
+                <input id="jk" name="jk" value="L" type="radio" class="form-control" required>Laki-laki
                 <input id="jk" name="jk" value="P" type="radio" class="form-control">Perempuan
             </div>
         </div>
@@ -52,7 +68,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-arrow-circle-up"></i>
                     </div>
                 </div>
-                <input id="tmp_lahir" name="tmp_lahir" value="" type="text" class="form-control">
+                <input id="tmp_lahir" name="tmp_lahir" value="<?php if(isset($row['tmp_lahir'])) echo $row['tmp_lahir']; ?>" type="text" class="form-control">
             </div>
         </div>
     </div>
@@ -65,7 +81,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-arrow-circle-right"></i>
                     </div>
                 </div>
-                <input id="tgl_lahir" name="tgl_lahir" value="" type="date" class="form-control">
+                <input id="tgl_lahir" name="tgl_lahir" value="<?php if(isset($row['tgl_lahir'])) echo $row['tgl_lahir'];?>" type="date" class="form-control">
             </div>
         </div>
     </div>
@@ -78,7 +94,7 @@ require_once '../dbkoneksi.php';
                         <i class="fa fa-arrow-circle-right"></i>
                     </div>
                 </div>
-                <input id="email" name="email" value="" type="email" class="form-control">
+                <input id="email" name="email" value="<?php if(isset($row['email'])) echo $row['email'];?>" type="email" class="form-control">
             </div>
         </div>
     </div>
@@ -101,8 +117,13 @@ require_once '../dbkoneksi.php';
         </div>
     </div>
     <div class="form-group row">
-        <div class="offset-4 col-8">
-            <input type="submit" name="proses" type="submit" class="btn btn-primary" value="Simpan" />
-        </div>
+    <div class="offset-4 col-8">
+    <?php
+        $button = (empty($_id)) ? "Simpan":"Update"; 
+    ?>
+      <input type="submit" name="proses" type="submit" 
+      class="btn btn-primary" value="<?=$button?>"/>
+      <input type="hidden" name="idedit" value="<?=$_id?>"/>
     </div>
+  </div>
 </form>
